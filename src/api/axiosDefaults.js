19bf3bd299e87a0106fc2c0d19fa3c42b9319282
Alsona Natsi <alsona.naci1188@gmail.com>
2024-08-31 01:45:20 +0000

    navbar

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+axios.defaults.baseURL = " https://drf-api-alsona-0c809e0777a5.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
